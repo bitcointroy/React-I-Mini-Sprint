bcc11a0b890e4e748ee1b4b9f17457d79f0ca52d
U-Honolulu\Troy <me@bitcointroy.co>
2018-03-15 15:22:31 -0500

    Old homework

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,57 @@
 // Step 1: Import React, and Component from react.
+import React, { Component } from 'react';
 // Step 2: Import render from react-dom
 // Step 3: Import MoviesList from MoviesList
+import MoviesList from './MoviesList';
 
-// Step 4: Write a class called App, extending Component.
-// Step 5: Write a constructor function without passing anything into it.
-// Step 6: Inside the constructor function, call super without passing anything into it.
-// Step 7: Inside the constructor function, create a state on this, and set it to an object.
-// Step 8: Inside the object, set a key to movies, and the value to an array of movies
-// Step 9: Write a render function.
-// Step 10: Inside the render function, write a return statement.
-// Step 11: Inside the return statement, write a selfclosing tag called MoviesList
-// Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
+
+class App extends Component {    // Step 4: Write a class called App, extending Component.
+    constructor() {   // Step 5: Write a constructor function without passing anything into it.
+        super();    // Step 6: Inside the constructor function, call super without passing anything into it.
+        this.state = {
+            title: "",
+            movies: ['Predator', 'Star Wars Ep. 4', 'Ready Player One']
+        };   // Step 7: Inside the constructor function, create a state on this, and set it to an object.
+        // Step 8: Inside the object, set a key to movies, and the value to an array of movies
+        this.changeHandler = this.changeHandler.bind(this);
+        this.addMovieHandler = this.addMovieHandler.bind(this);
+    }
+    
+    changeHandler(event) {
+        this.setState({
+            title: event.target.value
+        });
+    }
+    
+    addMovieHandler() {
+        const title = this.state.title;
+        const movies = this.state.movies;
+        movies.push(title);
+        this.setState({
+            movies,
+            title: ""
+        });
+    }
+
+    render() {       // Step 9: Write a render function.
+        return (     // Step 10: Inside the render function, write a return statement.
+            <MoviesList
+            movies={this.state.movies}
+            title={this.state.title}
+            changeHandler={this.changeHandler}
+            addMovieHandler={this.addMovieHandler}
+            />   // Step 11: Inside the return statement, write a selfclosing tag called MoviesList Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
+        );
+    }
+}
+
+export default App;
+
+
+// mount and unmount attach and detach elements to / from the DOM
+
+
+
+
+
 // Step 13: Outside the class, export the App class as a default.
